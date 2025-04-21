e838a05971c68f4a6d1554917a7b3f9926b14431
Malavika <malavika.selvan@guhatek.com>
2025-04-21 15:05:41 +0000

    changes

--- frontend/app.js
+++ frontend/app.js
@@ -1,14 +1,10 @@
-fetch("/api/books")
+fetch("http://backend:80/books")
   .then(res => res.json())
   .then(books => {
     const list = document.getElementById("book-list");
     books.forEach(book => {
       const li = document.createElement("li");
-      const link = document.createElement("a");
-      link.href = book.url;
-      link.textContent = `${book.title} by ${book.author}`;
-      link.target = "_blank"; // opens in new tab
-      li.appendChild(link);
+      li.textContent = `${book.title} by ${book.author}`;
       list.appendChild(li);
     });
   });
